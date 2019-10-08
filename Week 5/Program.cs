89228489a16a40354be05ddb7972591a7a5b9794
Christopher Sullivan <S00188845@mail.itsligo.ie>
2019-10-08 16:35:15 +0100

    Add project files.

---
+++ Week 5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Team> teams = new List<Team>();
+
+            Team Arsenal = new Team("Arsenal");
+            Team Liverpool = new Team("Liverpool");
+            Team Everton = new Team("Everton");
+            Team Barcelona = new Team("Barcelona");
+
+            teams.Add(Arsenal);
+            teams.Add(Liverpool);
+            teams.Add(Everton);
+            teams.Add(Barcelona);
+
+            Display(teams);
+
+            Arsenal.AddResult(Result.Win);
+            Liverpool.AddResult(Result.Draw);
+            Everton.AddResult(Result.Loss);
+            Barcelona.AddResult(Result.Win);
+
+            Display(teams);
+
+            teams.Sort();
+            
+            Display(teams);
+
+            teams.Reverse();
+
+            Display(teams);
+
+            Arsenal.Players.Add(new Player() { Name = "Ed McGinty", Position = "Goalkeeper" });
+            Arsenal.Players.Add(new Player() { Name = "John Mahon", Position = "Defender" });
+            Arsenal.Players.Add(new Player() { Name = "Ronan Coughlan", Position = "Forward" });
+
+            Console.WriteLine("\nArsenal Teamsheet");
+            foreach(Player player in Arsenal.Players)
+            {
+                
+                Console.WriteLine(player);
+            }
+        }
+        static void Display(List<Team> teams)
+        {
+            Console.WriteLine("{0, -15}{1, -7}{2, -7}{3, -7}{4,-7}{5, -7}", "Name", "Points", "Wins", "Draws", "Losses", "Played");
+
+            foreach (Team team in teams)
+            {
+                Console.WriteLine(team.DisplayTeamTable());
+            }
+        }
+
+
+    }
+}
---
+++ Week 5/Team.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_5
+{
+    public enum Result { Win, Draw, Loss}
+    public class Team: IComparable
+    {
+        public string Name { get; set; }
+        public int NumWins { get; set; }
+        public int NumDraws { get; set; }
+        public int NumLoss { get; set; }
+        public int NumGames { get; set; }
+
+        private int points;
+
+        public int Points
+        {
+            get
+            {
+                points = (NumWins * 3) + (NumDraws * 1);
+                return points;
+            }
+        }
+
+        public List<Player> Players { get; set; }
+
+        public Team(string name)
+        {
+            Name = name;
+            Players = new List<Player>();
+        }
+
+        public string DisplayTeamTable()
+        {
+            return ($"{Name,-15}{Points,-7}{NumWins,-7}{NumDraws,-7}{NumLoss,-7}{NumGames,-7}");
+        }
+
+        public void AddResult(Result result)
+        {
+            NumGames++;
+            switch (result)
+            {
+                case Result.Win:
+                    NumWins++;
+                    break;
+
+                case Result.Loss:
+                    NumLoss++;
+                    break;
+
+                case Result.Draw:
+                    NumDraws++;
+                    break;
+            }
+            
+           
+
+        }
+
+        public int CompareTo(object obj)
+        {
+            Team that = (Team)obj;
+            return points.CompareTo(that.Points);
+        }
+    }
+}
